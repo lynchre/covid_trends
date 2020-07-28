9e56b6c837db8b6a682f61d3dcf0ddb754686078
lynchre <lynchre@umich.edu>
2020-07-27 21:22:47 -0400

    sources, next steps

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-async function init() {
+function init() {
     // initialize SVG components, variables
     var width = 500,
         height = 600,
@@ -17,11 +17,15 @@ async function init() {
                     .domain([0,100])
                     .range(['#A4D263', '#E8DB3A', '#E89C3A', '#E83D3A']);
 
+    // load data... async 
     d3.csv('data/google-covid.csv', function(data) {
-        
-    })
+        // us geo json source: https://eric.clst.org/tech/usgeojson/
+        d3.json('data/us-states.json', function(json) {
+
+        })
+    });
     
 
-    
+    // http://duspviz.mit.edu/d3-workshop/mapping-data-with-d3/ for time slider reference
 
 }
